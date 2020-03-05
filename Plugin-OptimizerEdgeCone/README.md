2bdfba474e56aafad1275a8b6c07f75e2656eb55
Zeuphir <elias.wipfli@hotmail.com>
2020-03-05 10:10:26 +0100

    -

--- Plugin-OptimizerEdgeCone/README.md
+++ Plugin-OptimizerEdgeCone/README.md
@@ -1,1 +1,3 @@
 # Ba_Plugins
+
+Test
---
+++ Plugin-OptimizerEdgeCone/optimizerEdgeCone.hh
@@ -0,0 +1,13 @@
+//
+// Created by elias on 05.03.20.
+//
+
+#ifndef BA_GIT_OPTIMIZEREDGECONE_HH
+#define BA_GIT_OPTIMIZEREDGECONE_HH
+
+#include <OpenFlipper/BasePlugin/BaseInterface.hh>
+#include <OpenFlipper/common/Types.hh>
+
+class OptimizerEdgeCone : public QObject, BaseInterface
+
+#endif //BA_GIT_OPTIMIZEREDGECONE_HH
